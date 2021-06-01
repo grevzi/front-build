84bfff222bb3954891bc9f8f5f59ccc472dbbdd1
Roman <grevzi@gmail.com>
2021-06-01 15:57:03 +0300

    Added JIT mode for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  mode: 'jit',
+  purge: [
+    './dist/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+    './src/twig/**/*.{twig}',
+    './src/pug/**/*.{pug}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
